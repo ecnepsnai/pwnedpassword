d9cdea387b916775606bd3c907dee7da3f62a166
Ian Spence <ian@ecn.io>
2019-03-22 19:34:15 -0700

    Improved tests

--- pwned_test.go
+++ pwned_test.go
@@ -3,44 +3,88 @@ package pwned
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"sync"
 	"testing"
 )
 
-func TestIsPwned(t *testing.T) {
-	t.Run("Test Positive Match", func(t *testing.T) {
-		rt, err := IsPwned("password")
-		if err != nil {
-			t.Errorf("IsPwned() error = %s", err.Error())
-			return
-		}
+func TestPositiveMatch(t *testing.T) {
+	t.Parallel()
 
-		if !rt.Pwned {
-			t.Errorf("TestIsPwned fail: want Pwned: true got, Pwned: false")
-		}
+	rt, err := IsPwned("password")
+	if err != nil {
+		t.Errorf("IsPwned() error = %s", err.Error())
+		return
+	}
 
-		if rt.TimesObserved == 0 {
-			t.Errorf("TestIsPwned fail: want TimesObserved: 0 got, TimesObserved: %d", rt.TimesObserved)
-		}
+	if !rt.Pwned {
+		t.Errorf("TestIsPwned fail: want Pwned: true got, Pwned: false")
+	}
+
+	if rt.TimesObserved == 0 {
+		t.Errorf("TestIsPwned fail: want TimesObserved: 0 got, TimesObserved: %d", rt.TimesObserved)
+	}
+}
+
+func TestNegativeMatch(t *testing.T) {
+	t.Parallel()
+
+	// Generate Random Password
+	randB := make([]byte, 32)
+	rand.Read(randB)
+	password := base64.StdEncoding.EncodeToString(randB)
+
+	rt, err := IsPwned(password)
+	if err != nil {
+		t.Errorf("IsPwned() error = %s", err.Error())
+		return
+	}
+
+	if rt.Pwned {
+		t.Errorf("TestIsPwned fail: want Pwned: false got, Pwned: true")
+	}
+
+	if rt.TimesObserved > 0 {
+		t.Errorf("TestIsPwned fail: want TimesObserved: >0 got, TimesObserved: 0")
+	}
+}
+
+func TestAsync(t *testing.T) {
+	t.Parallel()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	var result *Result
+	var err error
+
+	IsPwnedAsync("password", func(r *Result, e error) {
+		result = r
+		err = e
+		wg.Done()
 	})
 
-	t.Run("Test Negative Match", func(t *testing.T) {
-		// Generate Random Password
-		randB := make([]byte, 16)
-		rand.Read(randB)
-		password := base64.StdEncoding.EncodeToString(randB)
+	wg.Wait()
 
-		rt, err := IsPwned(password)
-		if err != nil {
-			t.Errorf("IsPwned() error = %s", err.Error())
-			return
-		}
+	if err != nil {
+		t.Errorf("IsPwned() error = %s", err.Error())
+		return
+	}
 
-		if rt.Pwned {
-			t.Errorf("TestIsPwned fail: want Pwned: false got, Pwned: true")
-		}
+	if !result.Pwned {
+		t.Errorf("TestIsPwned fail: want Pwned: true got, Pwned: false")
+	}
 
-		if rt.TimesObserved > 0 {
-			t.Errorf("TestIsPwned fail: want TimesObserved: >0 got, TimesObserved: 0")
-		}
-	})
+	if result.TimesObserved == 0 {
+		t.Errorf("TestIsPwned fail: want TimesObserved: 0 got, TimesObserved: %d", result.TimesObserved)
+	}
+}
+
+func TestEmptyPassword(t *testing.T) {
+	t.Parallel()
+
+	_, err := IsPwned("")
+	if err == nil {
+		t.Errorf("No error seen when one expected")
+		return
+	}
 }
